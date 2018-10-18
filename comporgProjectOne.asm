5ae6d5cf0374f31ce623d39642a0c243de61594c
kierabr1 <44127341+kierabr1@users.noreply.github.com>
2018-10-18 17:01:21 -0400

    Update comporgProjectOne - I changed all of "add" to "addi"

--- comporgProjectOne.asm
+++ comporgProjectOne.asm
@@ -8,19 +8,19 @@
       #li $v0, 1     #Code call for integers
       li $v0, 11     #Code call for printing
       
-      add $a0, $a0, 11     #Prints character 'K'
+      addi $a0, $a0, 11     #Prints character 'K'
       syscall
       
-      add $a0, $a0, 30    # Prints character 'i'
+      addi $a0, $a0, 30    # Prints character 'i'
       syscall            
       
-      add $a0, $a0, -4   #Prints character 'e'
+      addi $a0, $a0, -4   #Prints character 'e'
       syscall
       
-      add $a0, $a0, 49  #Prints character 'r'
+      addi $a0, $a0, 49  #Prints character 'r'
       syscall
       
-      add $a0, $a0, -17 #Prints character 'a'
+      addi $a0, $a0, -17 #Prints character 'a'
       syscall
       
     
